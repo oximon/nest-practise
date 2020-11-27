8c2b2e4271480c515dd19b9e164ffa87d22c5490
Polyakov Semion <s_Qin4ik@live.ru>
2020-11-27 17:43:03 +0400

    added useQuard for product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,36 +6,43 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductService } from './product.service';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './interfaces/product.interface';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   getAll(): Product[] {
     return this.productService.getAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   getOne(@Param('id') id: string): Product {
     return this.productService.getOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createProductDto: CreateProductDto): Product {
     return this.productService.create(createProductDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(id, updateProductDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string): Product {
     return this.productService.remove(id);
